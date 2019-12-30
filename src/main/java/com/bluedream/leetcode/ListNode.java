c2bff5e32a0c62a1767758cc58acbae65fd15e8e
liuwenxin03 <liuwenxin03@meituan.com>
2019-12-30 23:39:06 +0800

    leetcode 第二题 两数相加

---
+++ src/main/java/com/bluedream/leetcode/ListNode.java
@@ -0,0 +1,18 @@
+package com.bluedream.leetcode;
+
+/**
+ * @author: 刘文鑫(liuwenxin03)
+ * @date: 2019-12-30 23:11
+ * @desc:
+ */
+public class ListNode {
+
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
