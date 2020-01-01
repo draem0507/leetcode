78465acdb5bbd97d5de6a98a7ecaf97fa2aed8c5
liuwenxin03 <liuwenxin03@meituan.com>
2020-01-01 20:39:52 +0800

    leetcode 简单5题

---
+++ src/main/java/com/bluedream/leetcode/AddBinary.java
@@ -0,0 +1,42 @@
+package com.bluedream.leetcode;
+
+/**
+ * @author: 刘文鑫(liuwenxin03)
+ * @date: 2020-01-01 19:57
+ * @desc: 二进制求和
+ */
+public class AddBinary {
+
+    public String addBinary(String a, String b) {
+
+        int alen = a.length() - 1;
+        int blen = b.length() - 1;
+        int num = 0;
+        StringBuffer sb = new StringBuffer();
+        while (alen >= 0 || blen >= 0 || num != 0) {
+
+            int first = alen >= 0 ? Integer.parseInt(a.charAt(alen--) + "") : 0;
+            int second = blen >= 0 ? Integer.parseInt(b.charAt(blen--) + "") : 0;
+            int sum = first + second + num;
+            num = sum / 2;
+            sum = sum % 2;
+
+            sb.append(sum);
+
+
+        }
+
+        return sb.reverse().toString();
+
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(2 / 2);
+
+        //  new AddBinary().addBinary("11","1");
+    }
+
+
+}
---
+++ src/main/java/com/bluedream/leetcode/MaxSubArray.java
@@ -0,0 +1,28 @@
+package com.bluedream.leetcode;
+
+/**
+ * @author: 刘文鑫(liuwenxin03)
+ * @date: 2020-01-01 18:21
+ * @desc: 最大子序和
+ */
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int res=nums[0];
+
+        for(int num :nums){
+            if(sum>=0){
+                sum+=num;
+            }else {
+                sum=num;
+            }
+            res =Math.max(res,sum);
+
+        }
+        return res;
+
+
+
+    }
+}
